4942d2a1b02a5a38ac66b5790272f9bbf10b7034
Matt <mattelliottus@gmail.com>
2019-07-08 09:31:53 -0700

    wrote control flow so functions can be called without exporting using node, not npm run

--- liri.js
+++ liri.js
@@ -8,35 +8,55 @@ const logSongData = require('./utils.js').logSongData;
 const spotify = new Spotify(keys.spotify);
 
 
-module.exports.concertThis = function() {
-  let url = `https://rest.bandsintown.com/artists/${process.argv[1]}/events?app_id=codingbootcamp`;
+function concertThis(artist) {
+  let url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
 
-  axios.get(url).then(function(response) {
+  axios.get(url).then(function (response) {
     let data = response.data;
     logConcertData(data);
   });
 };
 
-module.exports.spotifyThisSong = function() {
-  let query = process.argv[1] != undefined ? process.argv[1] : 'The Sign Ace of Base';
+function spotifyThisSong(song) {
+  let query = song != undefined ? song : 'The Sign Ace of Base';
 
   spotify.search({ type: 'track', query: query, limit: 1 })
     .then(function (data) {
-    if(data.tracks.total === 0) {
-      console.error('No Results Found. Please try again!');
-      return;
-    }
+      if (data.tracks.total === 0) {
+        console.error('No Results Found. Please try again!');
+        return;
+      }
 
-    logSongData(data.tracks.items);
-  }, function(error) {
-    console.error('error', error);
-  });
+      logSongData(data.tracks.items);
+    }, function (error) {
+      console.error('error', error);
+    });
 };
 
-module.exports.movieThis = function() {
+function movieThis() {
   console.log(' movie')
 };
 
-module.exports.doWhatItSays = function() {
+function doWhatItSays() {
 
 }
+
+//** Listen for CL commands **\\
+(function () {
+  if (require.main != module) return;
+
+  switch (process.argv[2]) {
+    case 'concert-this':
+      concertThis(process.argv[3]);
+      break;
+    case 'spotify-this-song':
+      spotifyThisSong(process.argv[3]);
+      break;
+    case 'movie-this':
+      movieThis();
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+  }
+})();
